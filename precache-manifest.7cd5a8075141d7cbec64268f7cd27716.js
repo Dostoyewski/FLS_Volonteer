ac15da2c570cb8b344b770b9d44ec3b87a708208
Dostoyewski <dostoyewski@yandex.ru>
2019-10-26 15:47:59 +0300

    Updates

---
+++ precache-manifest.7cd5a8075141d7cbec64268f7cd27716.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "859d5c1b0bf7b17d84ee36c1ab990e9d",
+    "url": "/FLS_Volonteer/index.html"
+  },
+  {
+    "revision": "094f0beac8b915889feb",
+    "url": "/FLS_Volonteer/static/css/2.27cde75d.chunk.css"
+  },
+  {
+    "revision": "6004801a8edb84749314",
+    "url": "/FLS_Volonteer/static/css/main.00aaeace.chunk.css"
+  },
+  {
+    "revision": "094f0beac8b915889feb",
+    "url": "/FLS_Volonteer/static/js/2.23c836c7.chunk.js"
+  },
+  {
+    "revision": "6004801a8edb84749314",
+    "url": "/FLS_Volonteer/static/js/main.3673defd.chunk.js"
+  },
+  {
+    "revision": "5386e17aefb37bf52929",
+    "url": "/FLS_Volonteer/static/js/runtime-main.f68ef261.js"
+  }
+]);
